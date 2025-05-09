bb7bb7bb85aea975b84df9fcccdca03e7d56a37d
kiwidoggie <forums@allenthinks.com>
2025-05-09 17:47:53 -0400

    mutex: Update mutex to use std::mutex

--- src/dice/hfe/Mutex.hpp
+++ src/dice/hfe/Mutex.hpp
@@ -1,42 +1,18 @@
-#ifdef _WIN32
-#include <windows.h>
-#else
-#include <pthread.h>
-#endif
+#pragma once
+#include <mutex>
 
-namespace dice {
-    namespace hfe {
+namespace dice 
+{
+    namespace hfe 
+    {
+        class Mutex 
+        {
+        public:
+            std::mutex m_Mutex;
 
-        class Mutex {
         public:
             Mutex();
             ~Mutex();
-
-        private:
-#ifdef _WIN32
-            HANDLE mutex_;
-#else
-            pthread_mutex_t mutex_;
-#endif
         };
-
-        Mutex::Mutex() {
-#ifdef _WIN32
-            mutex_ = CreateMutex(nullptr, FALSE, nullptr);
-#else
-            pthread_mutex_init(&mutex_, nullptr);
-#endif
-        }
-
-        Mutex::~Mutex() {
-#ifdef _WIN32
-            if (mutex_) {
-                CloseHandle(mutex_);
-            }
-#else
-            pthread_mutex_destroy(&mutex_);
-#endif
-        }
-
     }
 }
