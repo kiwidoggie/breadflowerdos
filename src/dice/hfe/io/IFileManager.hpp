727c4efce92c97ac4cb11c373e9810fa42d9429d
FlashHit <56718716+FlashHit@users.noreply.github.com>
2025-05-20 02:45:39 +0200

    Add IFileSystem and IFileManager

---
+++ src/dice/hfe/io/IFileManager.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "IFileSystem.hpp"
+#include <vector>
+
+namespace dice
+{
+    namespace hfe
+    {
+        namespace io
+        {
+            class IArchiveHandler;
+            class IArchive;
+            enum class FileResolveMode;
+
+            const uint32_t IID_IFileManager = 0xCC0146FB;
+
+            class IFileManager : public IFileSystem
+            {
+            public:
+                virtual void setBaseSystem(IFileSystem*) = 0;
+                virtual IFileSystem* getBaseSystem() = 0;
+                virtual IStream* openwin32(std::string const&, FileMode, FileAccess, uint32_t) = 0;
+                virtual void addArchiveHandler(IArchiveHandler*) = 0;
+                virtual void removeArchiveHandler(IArchiveHandler*) = 0;
+                virtual bool addPathOverload(std::string const&, std::string const&) = 0;
+                virtual bool removePathOverload(std::string const&, std::string const&) = 0;
+                virtual bool addMergeDirectory(std::string const&, std::string const&) = 0;
+                virtual bool removeMergeDirectory(std::string const&) = 0;
+                virtual uint32_t openArchive(std::string const&, bool) = 0;
+                virtual void permitMountArchives(bool) = 0;
+                virtual IArchive* mountArchive(std::string const&, std::string const&, bool) = 0;
+                virtual bool unmountArchive(std::string const&) = 0;
+                virtual void unmountAllArchives() = 0;
+                virtual void getAllArchives(std::vector<IArchive*>*) = 0;
+                virtual bool hasMountedArchives() const = 0;
+                virtual std::string const& stripModPath(std::string const&) = 0;
+                virtual void* getPathList() const = 0;
+                virtual void addPath(std::string const&) = 0;
+                virtual void removePath(std::string const&) = 0;
+                virtual void removeAllPaths() = 0;
+                virtual void getWritePath(std::string const&, std::string&) = 0;
+                virtual FileResolveMode getFileResolveMode() const = 0;
+                virtual void setFileResolveMode(FileResolveMode) = 0;
+                virtual std::string const& fileExistsEx(std::string const&, bool) = 0;
+                virtual std::string const& fileExistsEx2(std::string const&) = 0;
+            };
+        }
+    }
+}
---
+++ src/dice/hfe/io/IFileSystem.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <dice/hfe/IBase.hpp>
+#include <string>
+#include <list>
+
+namespace dice
+{
+    namespace hfe
+    {
+        namespace io
+        {
+            class FileAccess;
+            class FileMode;
+            class FileInfo;
+            class IStream;
+
+            const uint32_t IID_IFileSystem = 0x501611FE;
+
+            class IFileSystem : public IBase
+            {
+            public:
+                virtual void init() {}
+                virtual bool isLastFileOpenedFromArchive() const { return false; }
+                virtual IStream* open(std::string const&, FileMode, FileAccess, uint32_t) = 0;
+                virtual bool fileExists(std::string const&) = 0;
+                virtual bool deleteFile(std::string const&) = 0;
+                virtual bool moveFile(std::string const&, std::string const&) = 0;
+                virtual bool copyFile(std::string const&, std::string const&, bool) = 0;
+                virtual bool readFile(std::string const&, void*, uint32_t, uint32_t&) = 0;
+                virtual bool writeFile(std::string const&, void const*, uint32_t, bool) = 0;
+                virtual uint32_t findFiles(std::string const&, bool, std::list<FileInfo>&) = 0;
+                virtual bool getFileInfo(std::string const&, FileInfo&) = 0;
+                virtual bool compareFileTime(std::string const&, std::string const&) = 0;
+                virtual bool getFullFileName(std::string&) = 0;
+                virtual void update() = 0;
+                virtual void getHomeSubstitution() const = 0;
+                virtual void getRootSubstitution() const = 0;
+            };
+        }
+    }
+}
