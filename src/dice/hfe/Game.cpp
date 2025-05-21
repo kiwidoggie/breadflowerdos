b162bfd1be4fc70610bbc52b932bf999aea4ea65
FlashHit <56718716+FlashHit@users.noreply.github.com>
2025-05-22 01:54:16 +0200

    update Game class

--- src/dice/hfe/Game.cpp
+++ src/dice/hfe/Game.cpp
@@ -9,3 +9,120 @@ MapInfo* Game::getMapInfo(MapInfo* mapInfo)
 	*mapInfo = m_currentMap;
 	return mapInfo;
 }
+
+bool Game::localRadioMessage(TargetChannel, std::string const&)
+{
+	// TODO: Implement
+	return true;
+}
+
+bool Game::localGameInfoMessage(TargetChannel, std::string const&)
+{
+	// TODO: Implement
+	return true;
+}
+
+void Game::addPlayerInput(io::PlayerInput const&)
+{
+	// TODO: Implement
+}
+
+void* Game::getObjectFromNetworkableId(uint16_t const&)
+{
+	// TODO: Implement
+	return nullptr;
+}
+
+void Game::createAndDestroyObject(world::IObjectTemplate const*)
+{
+	// TODO: Implement
+}
+
+bool Game::destroyObject(world::IObject*)
+{
+	// TODO: Implement
+	return false;
+}
+
+void Game::joinSquad(int32_t)
+{
+	// TODO: Implement
+}
+
+void Game::leaveSquad(int32_t)
+{
+	// TODO: Implement
+}
+
+std::string const& Game::listSquadMembers(int32_t, int32_t)
+{
+	// TODO: Implement
+	return "";
+}
+
+std::string const& dice::hfe::Game::listSquads(int32_t)
+{
+	// TODO: Implement
+	return "";
+}
+
+void Game::changeSquadName(std::string const&, bool)
+{
+	// TODO: Implement
+}
+
+void Game::makeMeCommander()
+{
+	// TODO: Implement
+}
+
+void Game::popNextCommanderApplicant(int32_t)
+{
+	// TODO: Implement
+}
+
+std::string const& Game::getSpottedMessage(uint16_t)
+{
+	// TODO: Implement
+	return "";
+}
+
+void Game::sendSpottedMessage(world::IPlayer*, uint16_t, Vec2, world::VehicleType)
+{
+	// TODO: Implement
+}
+
+void Game::callForSpecialist(world::IPlayer*, HudNodeTypes)
+{
+	// TODO: Implement
+}
+
+std::string const& Game::getCurrentMapName() const
+{
+	return m_currentMap.getMapName();
+}
+
+std::string const& Game::getCurrentMapGameMode() const
+{
+	return m_currentMap.getGameMode();
+}
+
+std::string const& Game::getCurrentMapLevelPath() const
+{
+	return m_currentMap.getLevelPath();
+}
+
+int32_t Game::getCurrentMapMaxPlayers() const
+{
+	return m_currentMap.getMaxNumberOfPlayers();
+}
+
+uint32_t Game::getCurrentMapSize() const
+{
+	return m_currentMapSize;
+}
+
+void Game::setCurrentMapSize(uint32_t mapSize)
+{
+	m_currentMapSize = mapSize;
+}
--- src/dice/hfe/Game.hpp
+++ src/dice/hfe/Game.hpp
@@ -6,17 +6,24 @@ namespace dice
 {
     namespace hfe
     {
-        struct MapInfo {
+        class MapInfo {
+        public:
             std::string m_gamemode;
             std::string m_levelPath;
             std::string m_mapName;
-            int m_maxPlayers;
+            int32_t m_maxPlayers;
             bool m_commanderEnabled;
             int m_challengeOrdinal;
             bool _0x24;
             bool _0x25;
             bool m_netBufferNeedsUpdate; // 0x26
             void* m_netBuffer; // 0x28
+
+        public:
+            std::string const& getGameMode() const { return m_gamemode; }
+            std::string const& getLevelPath() const { return m_levelPath; }
+            std::string const& getMapName() const { return m_mapName; }
+            int32_t getMaxNumberOfPlayers() const { return m_maxPlayers; }
         };
 
         const uint32_t CID_Game = 0x1D8A9;
@@ -24,21 +31,49 @@ namespace dice
         class Game : public IGame {
         public:
             MapInfo m_currentMap;
-            char _0x38[0x30];
+            char _0x38[0x10];
+            uint32_t m_currentMapSize;
+            char _0x4C[0x1C];
             TargetChannel m_currentChannel;
             ChannelMessageType m_channelMessageType;
             int32_t m_currentFlags;
-            char _0x74[0xC];
+            bool m_abortLoadLevel;
+            char _0x75[0xB];
 
         public:
-            virtual ~Game() {}
-            virtual bool load() { return false; }
-            virtual bool loadPath(std::string const&) { return false; }
-            virtual int32_t getCurrentFlags() { return m_currentFlags; }
-            virtual void setCurrentFlags(int32_t flags) { m_currentFlags = flags; }
-            virtual ChannelMessageType getCurrentType() { return m_channelMessageType; }
-            virtual void setCurrentType(ChannelMessageType msgType) { m_channelMessageType = msgType; }
-            virtual MapInfo* getMapInfo(MapInfo*);
+            virtual ~Game() override {}
+            virtual bool load() override { return false; }
+            virtual bool loadPath(std::string const&) override { return false; }
+            virtual int32_t getCurrentFlags() override { return m_currentFlags; }
+            virtual void setCurrentFlags(int32_t flags) override { m_currentFlags = flags; }
+            virtual ChannelMessageType getCurrentType() override { return m_channelMessageType; }
+            virtual void setCurrentType(ChannelMessageType msgType) override { m_channelMessageType = msgType; }
+            virtual MapInfo* getMapInfo(MapInfo*) override;
+            virtual bool localRadioMessage(TargetChannel, std::string const&) override;
+            virtual bool localGameInfoMessage(TargetChannel, std::string const&) override;
+            virtual void addPlayerInput(io::PlayerInput const&) override;
+            virtual int32_t getCurrentInputIndex() override { return 0; }
+            virtual void* getObjectFromNetworkableId(uint16_t const&) override;
+            virtual void createAndDestroyObject(world::IObjectTemplate const*) override;
+            virtual bool destroyObject(world::IObject*) override;
+            virtual void joinSquad(int32_t) override;
+            virtual void leaveSquad(int32_t) override;
+            virtual std::string const& listSquadMembers(int32_t, int32_t) override;
+            virtual std::string const& listSquads(int32_t) override;
+            virtual void changeSquadName(std::string const&, bool) override;
+            virtual void makeMeCommander() override;
+            virtual void popNextCommanderApplicant(int32_t) override;
+            virtual std::string const& getSpottedMessage(uint16_t) override;
+            virtual void sendSpottedMessage(world::IPlayer*, uint16_t, Vec2, world::VehicleType) override;
+            virtual void callForSpecialist(world::IPlayer*, HudNodeTypes) override;
+            virtual std::string const& getCurrentMapName() const override;
+            virtual std::string const& getCurrentMapGameMode() const override;
+            virtual std::string const& getCurrentMapLevelPath() const override;
+            virtual int32_t getCurrentMapMaxPlayers() const override;
+            virtual uint32_t getCurrentMapSize() const override;
+            virtual void setCurrentMapSize(uint32_t) override;
+            virtual void failedExitVehicle(int32_t) override {}
+            virtual bool getAbortLoadLevel() const override { return m_abortLoadLevel; }
         };
 
         extern Game* g_game;
@@ -47,6 +82,7 @@ namespace dice
         // TODO: Win32 static_assert
 #else
         static_assert(sizeof(Game) == 0x80);
+        static_assert(offsetof(Game, m_currentMapSize) == 0x48);
         static_assert(sizeof(MapInfo) == 0x30);
 #endif
     }
